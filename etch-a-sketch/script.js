5628d39cf7d100ecc1bd7fb9cd96b0221ed9d162
Kelsi Proulx <Kelsiproulx1919@gmail.com>
2021-03-24 20:20:55 -0700

    Setup grid with random color on hover and reset button to remove colors

---
+++ etch-a-sketch/script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById('container');
+const resetBtn = document.getElementById('btn');
+const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71'];
+let SQUARES = 256;
+
+for (let i = 0; i < SQUARES; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+
+  container.appendChild(square);
+
+  resetBtn.addEventListener('click', () => {
+    square.style.background = '#1d1d1d';
+    square.style.boxShadow = '0 0 2px #000'; 
+  });
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
